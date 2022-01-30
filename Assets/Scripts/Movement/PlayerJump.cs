19e8c186285f6a6959f33ee562d401a1e25876cc
FernandoMSoares <53447032+FernandoMSoares@users.noreply.github.com>
2022-01-30 23:42:32 +0000

    Player Jump refractor, Jumping animation
    Player Jump refractor: Changed grounding detection to Capsule check so it detects better when standing on edges.
    Added Jumping Animation on a separate layer to PlayerController animator.

--- Assets/Scripts/Movement/PlayerJump.cs
+++ Assets/Scripts/Movement/PlayerJump.cs
@@ -6,37 +6,53 @@ namespace Movement
     public class PlayerJump : MonoBehaviour
     {
         [Header("Values")] 
-        [SerializeField] private float jumpForce = 275f;
+        [SerializeField] private float jumpForce = 5f;
 
         [Header("Setup")] 
         [SerializeField] private LayerMask groundLayer;
     
-        [Tooltip("The origin of the ground check ray")] 
+        [Tooltip("The origin of the ground check capsule")] 
         [SerializeField] private Vector3 groundCheckRayOrigin = new Vector3(0,0.05f,0);
-        [Tooltip("The max distance the ground check ray will detect (tolerance)")] 
-        [SerializeField] private float groundCheckDistance = .5f;
+        [Tooltip("The max distance the ground check Capsule will detect (tolerance)")] 
+        [SerializeField] private float groundCheckDistance = 0.01f;
+
+        [SerializeField] private Animator animator;
 
         private Rigidbody rb;
-
-        private bool isGrounded;
-
-        private void Awake() => rb = GetComponent<Rigidbody>();
+        private float capsuleRadius;
+        
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+            capsuleRadius = GetComponent<CapsuleCollider>().radius;
+        }
 
         private void Update()
         {
-            //Checks if the player is touching the ground (an object with a collider and marked with the "Ground" layer)
-            isGrounded = Physics.Raycast(transform.position + groundCheckRayOrigin, Vector3.down, groundCheckDistance, groundLayer);
+            if (Input.GetKeyDown(KeyCode.Space) && IsGrounded()) 
+                Jump();
+            
+            animator.SetBool("Grounded", IsGrounded());
+        }
 
-            //If its grounded and we pressed jump, apply the force on rigidbody to jump
-            if (Input.GetKeyDown(KeyCode.Space) && isGrounded) 
-                rb.AddForce(Vector3.up * jumpForce);
+        private void Jump()
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+        private bool IsGrounded()
+        {
+            return Physics.CheckCapsule(groundCheckRayOrigin + transform.position, transform.position + Vector3.down *
+                groundCheckDistance, capsuleRadius, groundLayer);
         }
 
         private void OnDrawGizmos()
         {
-            //Draws the ground check ray for visualization, if the player is grounded the ray is green otherwise is red
-            Gizmos.color = isGrounded ? Color.green : Color.red;
-            Gizmos.DrawLine( transform.position + groundCheckRayOrigin, transform.position + groundCheckRayOrigin + new Vector3(0, -groundCheckDistance, 0));
+            //Draws the ground check rays for visualization, if the player is grounded the ray is green otherwise is red
+            Gizmos.color = IsGrounded() ? Color.green : Color.red;
+            Gizmos.DrawLine( transform.position + groundCheckRayOrigin, transform.position + Vector3.down * groundCheckDistance);
+            Gizmos.DrawLine( transform.position - new Vector3(capsuleRadius,0,0),  transform.position + new Vector3(capsuleRadius,0,0));
+            Gizmos.DrawLine( transform.position - new Vector3(0,0,capsuleRadius),  transform.position + new Vector3(0,0,capsuleRadius));
         }
     }
 }
