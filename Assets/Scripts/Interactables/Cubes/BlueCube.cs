2c4345b242d4d6ef88b3ab4a454ea11ec8bed4ed
FernandoMSoares <53447032+FernandoMSoares@users.noreply.github.com>
2022-02-03 16:04:58 +0000

    Blue Cube, Project Organization

--- Assets/Scripts/Interactables/Cubes/BlueCube.cs
+++ Assets/Scripts/Interactables/Cubes/BlueCube.cs
@@ -1,7 +1,67 @@
+using System;
+using UnityEngine;
+
 namespace Interactables.Cubes
 {
     public class BlueCube : Interactable
     {
+        private static readonly int Raised = Animator.StringToHash("Raised");
         
+        [SerializeField] private float pushForce = 10f;
+        [SerializeField] private bool raised;
+
+        private Animator animator;
+
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        public override void InteractLeft()
+        {
+            base.InteractLeft();
+            RaiseCube();
+        }
+
+        public override void InteractRight()
+        {
+            base.InteractRight();
+            LowerCube();
+        }
+
+        private void RaiseCube()
+        {
+            if (raised)
+                return;
+            raised = true;
+            
+            animator.SetBool(Raised, raised);
+        }
+
+        private void LowerCube()
+        {
+            if (!raised)
+                return;
+            raised = false;
+            
+            animator.SetBool(Raised, raised);
+        }
+        
+        private void Bounce(Collider other)
+        {
+            other.GetComponent<Rigidbody>().AddForce(Vector3.up * pushForce, ForceMode.Impulse);
+            RaiseCube();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                if (raised)
+                    return;
+                
+                Bounce(other);
+            }
+        }
     }
 }
