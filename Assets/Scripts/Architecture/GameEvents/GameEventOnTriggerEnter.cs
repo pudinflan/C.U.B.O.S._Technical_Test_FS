3e967e8712592de5411320bd81ea90ffab03ed97
FernandoMSoares <53447032+FernandoMSoares@users.noreply.github.com>
2022-02-03 17:53:10 +0000

    DoorWay Prefab animation, GameEventOnTrigger

---
+++ Assets/Scripts/Architecture/GameEvents/GameEventOnTriggerEnter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Architecture.GameEvents
+{
+    public class GameEventOnTriggerEnter : MonoBehaviour
+    {
+        [SerializeField] private string triggerTag = "Player";
+        [SerializeField] private GameEvent triggerEvent;
+        [SerializeField] private bool triggerOnlyOnce = false;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(triggerTag))
+            {
+                triggerEvent?.Invoke();
+                
+                if (triggerOnlyOnce) 
+                    Destroy(this);
+            }
+        }
+    }
+}
