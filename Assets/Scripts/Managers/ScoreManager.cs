5eda8a369f9c14c06e467b77005539cfd8d6d154
FernandoMSoares <53447032+FernandoMSoares@users.noreply.github.com>
2022-02-03 22:46:37 +0000

    Post Processing, Score Ui that displays time.

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Architecture.Variables;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+   [SerializeField] private FloatVariable levelTime;
+
+   private void Awake()
+   {
+      ResetTime();
+   }
+
+   private void Update()
+   {
+      levelTime.Value += Time.deltaTime;
+   }
+
+   private void ResetTime()
+   {
+      levelTime.Value = 0;
+   }
+}
---
+++ Assets/Scripts/UI/DisplayTime.cs
@@ -0,0 +1,27 @@
+﻿using Architecture.Variables;
+using TMPro;
+using UnityEngine;
+
+/// <summary>
+/// Displays time on UI
+/// </summary>
+public class DisplayTime : MonoBehaviour
+{
+    [SerializeField] private FloatVariable levelTime;
+    [SerializeField] private TMP_Text levelTimeText;
+
+    private void FixedUpdate()
+    {
+        levelTimeText.SetText(FormatTime(levelTime.Value));
+    }
+
+    //Code by  Hellium
+    //http://answers.unity.com/answers/1476304/view.html
+    private string FormatTime( float time )
+    {
+        int minutes = (int) time / 60 ;
+        int seconds = (int) time - 60 * minutes;
+        int milliseconds = (int) (100 * (time - minutes * 60 - seconds));
+        return $"{minutes:00}:{seconds:00}:{milliseconds:00}";
+    }
+}
