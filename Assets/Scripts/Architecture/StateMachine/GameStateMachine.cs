a3632f1015dda2a908b2fa7232f3a1ea263172a7
FernandoMSoares <53447032+FernandoMSoares@users.noreply.github.com>
2022-02-04 14:58:21 +0000

    Loading Scene, project organization. GameState machine to load Loading Scene

--- Assets/Scripts/Architecture/StateMachine/GameStateMachine.cs
+++ Assets/Scripts/Architecture/StateMachine/GameStateMachine.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Architecture.StateMachine
 {
@@ -11,12 +13,16 @@ namespace Architecture.StateMachine
             stateMachine = new StateMachine();
 
             var menu = new Menu();
-            var loading = new LoadLevel();
+            var loading = new LoadLevel("DemoLevel");
             var play = new Play();
             var pause = new Pause();
             
             //loads our level
             stateMachine.SetState(loading);
+            
+            //transitions
+            //transitions from loading to play if loading operation is finished
+            stateMachine.AddTransition(loading,play, loading.Finished); 
         }
     }
 
@@ -52,17 +58,21 @@ namespace Architecture.StateMachine
 
     public class LoadLevel : IState
     {
+        private AsyncOperation operation = new AsyncOperation();
+
+        private readonly string sceneToLoad;
+
+        public bool Finished() => operation.isDone;
+        
+        public LoadLevel(string sceneName) => sceneToLoad = sceneName;
+
         public void OnStateUpdate()
         {
         }
 
-        public void OnStateEnter()
-        {
-        }
+        public void OnStateEnter() => operation = SceneManager.LoadSceneAsync(sceneToLoad);
 
-        public void OnStateExit()
-        {
-        }
+        public void OnStateExit() => operation = null;
     }
     
     public class Pause : IState
---
+++ Assets/Scripts/UI/Loading/TextBreathingEffect.cs
@@ -0,0 +1,15 @@
+﻿using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+namespace UI.Loading
+{
+    public class TextBreathingEffect : MonoBehaviour
+    {
+        private TMP_Text text;
+
+        private void Awake() => text = GetComponent<TMP_Text>();
+
+        private void Start() => text.DOFade(0.3f, 2f).SetLoops(-1, LoopType.Yoyo);
+    }
+}
