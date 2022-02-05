bc4a78266f1fb7c0498d6fec144081b05976cf29
FernandoMSoares <53447032+FernandoMSoares@users.noreply.github.com>
2022-02-05 09:49:09 +0000

    new Cube Textures and color correction.

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Audio
+{
+    public class AudioManager : MonoBehaviour
+    {
+        private static bool _initialized;
+        
+        [SerializeField] private audiomi
+        
+        void Awake()
+        {
+            if (_initialized)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            _initialized = true;
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+}
